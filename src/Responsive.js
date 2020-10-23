2b53040c956aeebc73e527bc4d3c6edec6cc22a9
JJonggTaeng <jjojjo141995@gmail.com>
2020-10-23 23:13:46 +0900

    반응형 수정

--- src/Responsive.js
+++ src/Responsive.js
@@ -5,7 +5,7 @@ const Responsive = () => {
   return(
     <div className="responsive-list">
       <div className="responsive-link">
-        <a href="https://github.com/JJongTaeng/3d-responsive" target="_blank">git : 3d-responsive</a>
+        <a href="https://github.com/JJongTaeng/firist-web-page" target="_blank">git : Responsive-webpage</a>
       </div>
       <div className="responsive-image responsive-image1"></div>
       <div className="responsive-image responsive-image2"></div>
